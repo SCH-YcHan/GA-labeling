104af3b0e466f38cd328a0de75a79e142e28bf67
SCH-YcHan <ychan.sch@gmail.com>
2023-11-25 22:51:53 +0900

    Code Modify

--- code_revision/05. Train labeling with Updown.R
+++ code_revision/05. Train labeling with Updown.R
@@ -25,3 +25,28 @@ for(symbol in symbols){
     cbind(Date=stock$Date, .) %>% 
     write.csv(paste0("../data/UD_label/", symbol, "_UD.csv"), row.names=F)
 }
+
+rm(list=ls())
+
+source("Labeling_code.R")
+
+KOSPI <- read.csv("../data/Stock_data/pre_KOSPI.csv")
+KOSPI$Date <- as.Date(KOSPI$Date)
+
+symbols <- read.csv("../data/KOSPI_Marketcap60.csv")$종목코드
+
+for(symbol in symbols){
+  stock <- KOSPI %>%
+    select(Date, paste(symbol, "Open", sep="_")) %>% 
+    filter(Date < "2019-01-01")
+  
+  names(stock) <- c("Date", "Open")
+  
+  stock %>% 
+    select(Open) %>% 
+    lapply(function(x){data.frame(label = UpDown(x, N=1))}) %>% 
+    do.call(cbind, .) %>% 
+    cbind(Date=stock$Date, .) %>% 
+    write.csv(paste0("../data/UD_label/", str_remove(symbol, "X"), "_UD.csv"), row.names=F)
+}
+
