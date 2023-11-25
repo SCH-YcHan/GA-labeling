f30e7f44d2fb6b690d9e8e9ef594bf905c1728ab
SCH-YcHan <ychan.sch@gmail.com>
2023-11-25 22:57:24 +0900

    Code modify

---
+++ code_revision/06. Train labeling with GA.R
@@ -0,0 +1,37 @@
+rm(list=ls())
+
+source("Objective function.R")
+
+NASDAQ <- read.csv("../data/Stock_data/pre_NASDAQ.csv")
+NASDAQ$Date <- as.Date(NASDAQ$Date)
+
+symbols <- read.csv("../data/NASDAQ_Marketcap60.csv")$Symbol
+
+if(!file.exists("../data/GA_RDS")){
+  dir.create("../data/GA_RDS")
+}
+
+for(symbol in symbols){
+  if(!file.exists(paste0("../data/GA_RDS/", symbol, "_paper.rds"))){
+    print(symbol)
+    stock <- NASDAQ %>%
+      select(Date, paste(symbol, "Open", sep="_")) %>% 
+      filter(Date < "2019-01-01")
+    
+    names(stock) <- c("Date", "Open")
+    
+    GA_paper <- ga(type="binary",
+                   fitness = obj_paper,
+                   nBits = nrow(stock),
+                   popSize = 300,
+                   maxiter = 1000,
+                   elitism = 3,
+                   seed = 20207188,
+                   parallel = T)
+    
+    saveRDS(GA_paper, paste0("../data/GA_RDS/", symbol, "_paper.rds"))
+    
+    gc()
+  }
+}
+
---
+++ code_revision/Objective function.R
@@ -0,0 +1,44 @@
+library(GA)
+library(dplyr)
+
+#Kim & Enke (2017)
+obj_paper <- function(stock_labels){
+  data <- cbind(stock, label=as.vector(stock_labels)+1) 
+  
+  data$label[nrow(data)]=2
+  
+  data2 <- data %>% 
+    dplyr::select(Date, label) %>%
+    dplyr::mutate(label = ifelse(label-dplyr::lag(label)!=0 | is.na(label-dplyr::lag(label)), label, 0)) %>%
+    dplyr::filter(label != 0) %>% 
+    merge(data %>% dplyr::mutate(Open = lead(Open)) %>% dplyr::select(Date, Open), by="Date", all=T) %>% 
+    dplyr::filter(!is.na(label)) %>% 
+    dplyr::filter(!is.na(Open))
+  
+  if((length(data2$label)==1 & data2$label[1]==2) |
+     (length(data2$label)==2 & data2$label[1]==2)){return(min(data$Open)-max(data$Open))}
+  if(data2$label[1]==2){data2$label[1]=NA}
+  
+  data3 <- data2 %>%
+    dplyr::filter(!is.na(label)) %>%
+    dplyr::mutate(profit = Open*0.9981-dplyr::lag(Open,1)*1.0019) %>% 
+    dplyr::filter(label == 2)
+  
+  N_trade <- nrow(data3)
+  Nw <- sum(data3$profit>0)
+  Wr <- Nw/N_trade
+  mean_W <- mean(data3$profit[data3$profit>0])
+  mean_L <- mean(abs(data3$profit[data3$profit<0]))
+  Pr <- mean_W/mean_L
+  Pf <- Pr*((Nw/(N_trade-Nw)))
+  
+  if(N_trade>30 & Wr>=0.33 & Wr<=0.80 & Pr>=1 & Pf>=1){
+    return((Wr*mean_W-(1-Wr)*mean_L)/mean_L*N_trade)
+  }else{
+    return(min(data$Open)-max(data$Open))
+  }
+}
+
+
+
+
